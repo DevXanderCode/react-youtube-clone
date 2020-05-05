3191235da785f8623497b08e898254d24ba84e67
Chinedu Alex <nwekealexchinedu@gmail.com>
2020-05-05 22:28:32 +0100

    Updates

---
+++ precache-manifest.70fc1723f8dd9a22fce206c2adb56047.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b02345ebbefc1f7592c6e22622d034e",
+    "url": "/react-youtube-clone/index.html"
+  },
+  {
+    "revision": "6a6dbe1c646af7339447",
+    "url": "/react-youtube-clone/static/js/2.2ad31804.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-youtube-clone/static/js/2.2ad31804.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c23f8790a660ddcbb09d",
+    "url": "/react-youtube-clone/static/js/main.9ffb0892.chunk.js"
+  },
+  {
+    "revision": "d28a466369fe810d0587",
+    "url": "/react-youtube-clone/static/js/runtime-main.662a6f75.js"
+  }
+]);
